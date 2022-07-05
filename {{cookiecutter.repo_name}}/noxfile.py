bba832f1c19fbb7e2a801e4f9f5b1d969e6f4cd1
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2022-07-05 14:31:14 +0200

    chore: add `twine` upload session

--- {{cookiecutter.repo_name}}/noxfile.py
+++ {{cookiecutter.repo_name}}/noxfile.py
@@ -28,3 +28,11 @@ def test(session: nox.Session):
 def whl(session: nox.Session):
     session.run(*"python -m setup bdist_wheel".split(" "))
 
+
+@nox.session
+def send(session: nox.Session):
+    session.install("twine")
+    token = session.env.get("TWINE_TOKEN")
+    if not token:
+        raise ValueError("PyPi upload token not set!")
+    session.run(*"twine upload dist/*.whl -u __token__ -p".split(" "), token)
